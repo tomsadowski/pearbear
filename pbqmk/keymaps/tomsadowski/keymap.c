94a34761ebafd148019b2affe244b214905e5a42
tomsadowski <tsadowski93@gmail.com>
2025-08-14 07:18:39 -0400

    2D game update for nine sols

--- pbqmk/keymaps/tomsadowski/keymap.c
+++ pbqmk/keymaps/tomsadowski/keymap.c
@@ -70,17 +70,21 @@ enum custom_keycodes
 
 enum layers
 {
+    // standard
     ALPHA_LYR,
     NUMBE_LYR,
     MOUSE_LYR,
+    // lefty
     LNAV1_LYR,
     LNAV2_LYR,
+    // game (no return)
     GAME2_LYR,
     GAME3_LYR,
-    ALPHA_FROM_NUMBE_LYR,
-    ALPHA_FROM_MOUSE_LYR,
-    NUMBE_FROM_OTHER_LYR,
-    MOUSE_FROM_OTHER_LYR,
+    // momentary
+    AFN_LYR,
+    AFM_LYR,
+    NFA_LYR,
+    MFA_LYR,
 };
 
 // switching to non-alpha layers breaks capsword
@@ -180,125 +184,115 @@ bool process_record_user(uint16_t keycode,
 
 // layers
 
-#define HOLD_MOUSE_TAP_SPACE            LT(MOUSE_FROM_OTHER_LYR, KC_SPC)
-#define HOLD_NUMBE_TAP_SPACE            LT(NUMBE_FROM_OTHER_LYR, KC_SPC)
-#define HOLD_ALPHA_TAP_SPACE_FROM_NUMBE LT(ALPHA_FROM_NUMBE_LYR, KC_SPC)
-#define HOLD_ALPHA_TAP_SPACE_FROM_MOUSE LT(ALPHA_FROM_MOUSE_LYR, KC_SPC)
+#define ATM_SPC LT(MFA_LYR, KC_SPC)
+#define ATN_SPC LT(NFA_LYR, KC_SPC)
+#define NTA_SPC LT(AFN_LYR, KC_SPC)
+#define MTA_SPC LT(AFM_LYR, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
-// alpha layer (home layer)
-// - contains: alphabet, space, backspace, delete
-[ALPHA_LYR] = LAYOUT_split_3x5_2(
-KC_K,    KC_C,    KC_H,    KC_B,    KC_X,     KC_Z,    KC_Y,    KC_W,    KC_P,    KC_V,
-KC_A,    KC_S,    KC_R,    KC_T,    KC_COMMA, KC_DOT,  KC_E,    KC_I,    KC_O,    KC_N,
-KC_J,    KC_F,    KC_L,    KC_D,    KC_QUOT,  KC_SCLN, KC_U,    KC_M,    KC_G,    KC_Q,
-         KC_BSPC, HOLD_MOUSE_TAP_SPACE,       HOLD_NUMBE_TAP_SPACE,       KC_DEL),
-// number layer
-// - contains: digits, arrow-keys, misc symbols, misc navigation
-[NUMBE_LYR] = LAYOUT_split_3x5_2(
-KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-KC_GRV,  KC_BSLS, KC_SLSH, KC_MINS, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-KC_INS,  KC_LBRC, KC_RBRC, KC_EQL,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                           _______, _______, HOLD_ALPHA_TAP_SPACE_FROM_NUMBE,  _______),
-// mouse layer
-// - contains: function keys 1-13, printscreen,
-// cursor direction/speed, scroll-wheel direction/speed,
-// left and right click
-[MOUSE_LYR] = LAYOUT_split_3x5_2(
-KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-KC_BTN6, KC_BTN5, KC_BTN4, KC_BTN3, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-KC_F11,  KC_F12,  KC_F13,  KC_PSCR, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-_______, HOLD_ALPHA_TAP_SPACE_FROM_MOUSE,    KC_BTN1, KC_BTN2),
-// lnav1 layer
-// - contains the navigation keys found in the number layer, but under the left hand
-[LNAV1_LYR] = LAYOUT_split_3x5_2(
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______,
-KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, _______, _______, _______, _______,
-                           _______, KC_SPC,  KC_SPC,  _______),
-// lnav2 layer
-// - contains the mouse keys found in the mouse layer, but under the left hand
-[LNAV2_LYR] = LAYOUT_split_3x5_2(
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______, _______,
-KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______, _______, _______, _______, _______,
-                           KC_BTN2, KC_BTN1, KC_SPC,  _______),
-// 2D game layer
-// - same as alpha layer with the following exceptions:
-//  - combos deactivated
-//  - KC_SPACE replaces HOLD_MOUSE_TAP_SPACE
-//  - KC_0 replaces HOLD_NUMBE_TAP_SPACE
-//  - KC_ESCAPE replaces KC_QUOTE
-//  - KC_ENTER replaces KC_SCLN
-[GAME2_LYR] = LAYOUT_split_3x5_2(
-_______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-_______, _______, _______, _______, KC_ESC,   KC_ENT,  _______, _______, _______, _______,
-                           _______, KC_SPC,   KC_0,    _______),
-// 3D game layer
-// - same as alpha layer with the following exceptions:
-//  - combos deactivated
-//  - KC_SPACE replaces HOLD_MOUSE_TAP_SPACE
-//  - KC_BTN1, KC_BTN2, replaces HOLD_NUMBE_TAP_SPACE, KC_DEL respectively
-//  - KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R replaces KC_E, KC_I, KC_O, KC_N respectively
-[GAME3_LYR] = LAYOUT_split_3x5_2(
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-_______, _______, _______, _______, KC_ESC,  KC_ENT,  _______, _______, _______, _______,
-                           _______, KC_SPC,  KC_BTN1, KC_BTN2),
-// alpha-from-number layer
-// - alpha layer momentarily accessed from number layer
-[ALPHA_FROM_NUMBE_LYR] = LAYOUT_split_3x5_2(
-KC_K,   KC_C,     KC_H,    KC_B,    KC_X,    KC_Z,    KC_Y,    KC_W,    KC_P,    KC_V,
-KC_A,   KC_S,     KC_R,    KC_T,    _______, _______, KC_E,    KC_I,    KC_O,    KC_N,
-KC_J,   KC_F,     KC_L,    KC_D,    _______, _______, KC_U,    KC_M,    KC_G,    KC_Q,
-                           _______, KC_SPC,  _______, _______),
-// alpha-from-mouse layer
-// - alpha layer momentarily accessed from mouse layer
-[ALPHA_FROM_MOUSE_LYR] = LAYOUT_split_3x5_2(
-KC_K,   KC_C,     KC_H,    KC_B,    KC_X,    KC_Z,    KC_Y,    KC_W,    KC_P,     KC_V,
-KC_A,   KC_S,     KC_R,    KC_T,    _______, _______, KC_E,    KC_I,    KC_O,     KC_N,
-KC_J,   KC_F,     KC_L,    KC_D,    _______, _______, KC_U,    KC_M,    KC_G,     KC_Q,
-                           _______, _______, HOLD_NUMBE_TAP_SPACE,      KC_DEL),
-// number-from-other layer
-// - number layer momentarily accessed from alpha layer or alpha-from-mouse layer
-[NUMBE_FROM_OTHER_LYR] = LAYOUT_split_3x5_2(
-KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-KC_GRV,  KC_BSLS, KC_SLSH, KC_MINS, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-KC_INS,  KC_LBRC, KC_RBRC, KC_EQL,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-                           _______, KC_SPC,  _______, _______),
-// mouse-from-other layer
-// - mouse layer momentarily accessed from alpha layer or number layer
-[MOUSE_FROM_OTHER_LYR] = LAYOUT_split_3x5_2(
-KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-KC_BTN6, KC_BTN5, KC_BTN4, KC_BTN3, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-KC_F11,  KC_F12,  KC_F13,  KC_PSCR, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-                           _______, _______, KC_BTN1, KC_BTN2),
+[ALPHA_LYR] = LAYOUT_split_3x5_2
+(
+KC_K   ,KC_C   ,KC_H   ,KC_B,   KC_X   ,KC_Z   ,KC_Y   ,KC_W   ,KC_P   ,KC_V   ,
+KC_A   ,KC_S   ,KC_R   ,KC_T,   KC_COMM,KC_DOT ,KC_E   ,KC_I   ,KC_O   ,KC_N   ,
+KC_J   ,KC_F   ,KC_L   ,KC_D,   KC_QUOT,KC_SCLN,KC_U   ,KC_M   ,KC_G   ,KC_Q   ,
+                        KC_BSPC,ATM_SPC,ATN_SPC,KC_DEL
+),
+[NUMBE_LYR] = LAYOUT_split_3x5_2
+(
+KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,
+KC_GRV ,KC_BSLS,KC_SLSH,KC_MINS,_______,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,
+KC_INS ,KC_LBRC,KC_RBRC,KC_EQL ,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,
+                        _______,_______,NTA_SPC,_______
+),
+[MOUSE_LYR] = LAYOUT_split_3x5_2
+(
+KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,
+KC_BTN6,KC_BTN5,KC_BTN4,KC_BTN3,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,
+KC_F11 ,KC_F12 ,KC_F13 ,KC_PSCR,_______,_______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,
+                        _______,MTA_SPC,KC_BTN1,KC_BTN2
+),
+[LNAV1_LYR] = LAYOUT_split_3x5_2
+(
+_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+KC_LEFT,KC_UP,  KC_DOWN,KC_RGHT,_______,_______,_______,_______,_______,_______,
+KC_HOME,KC_PGUP,KC_PGDN,KC_END ,_______,_______,_______,_______,_______,_______,
+                        _______,KC_SPC, KC_SPC, _______
+),
+[LNAV2_LYR] = LAYOUT_split_3x5_2
+(
+_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+KC_MS_L,KC_MS_U,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,
+KC_WH_L,KC_WH_U,KC_WH_D,KC_WH_R,_______,_______,_______,_______,_______,_______,
+                        KC_BTN2,KC_BTN1,KC_SPC ,_______
+),
+[GAME2_LYR] = LAYOUT_split_3x5_2
+(
+KC_A   ,KC_W   ,KC_S   ,KC_D   ,_______,_______,_______,KC_C   ,_______,_______,
+KC_LEFT,KC_UP,  KC_DOWN,KC_RGHT,_______,KC_H   ,_______,_______,_______,_______,
+_______,_______,_______,KC_B   ,KC_ESC ,KC_ENT ,_______,_______,_______,_______,
+                        KC_K   ,KC_SPC ,KC_R   ,KC_T
+),
+
+[GAME3_LYR] = LAYOUT_split_3x5_2
+(
+_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+_______,_______,_______,_______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,
+_______,_______,_______,_______,KC_ESC ,KC_ENT ,_______,_______,_______,_______,
+                        _______,KC_SPC ,KC_BTN1,KC_BTN2
+),
+[AFN_LYR] = LAYOUT_split_3x5_2
+(
+KC_K   ,KC_C   ,KC_H   ,KC_B   ,KC_X   ,KC_Z   ,KC_Y   ,KC_W   ,KC_P   ,KC_V   ,
+KC_A   ,KC_S   ,KC_R   ,KC_T   ,_______,_______,KC_E   ,KC_I   ,KC_O   ,KC_N   ,
+KC_J   ,KC_F   ,KC_L   ,KC_D   ,_______,_______,KC_U   ,KC_M   ,KC_G   ,KC_Q   ,
+                        _______,KC_SPC, _______,_______
+),
+[AFM_LYR] = LAYOUT_split_3x5_2
+(
+KC_K   ,KC_C   ,KC_H   ,KC_B   ,KC_X   ,KC_Z   ,KC_Y   ,KC_W   ,KC_P   ,KC_V   ,
+KC_A   ,KC_S   ,KC_R   ,KC_T   ,_______,_______,KC_E   ,KC_I   ,KC_O   ,KC_N   ,
+KC_J   ,KC_F   ,KC_L   ,KC_D   ,_______,_______,KC_U   ,KC_M   ,KC_G   ,KC_Q   ,
+                        _______,_______,ATN_SPC,KC_DEL
+),
+[NFA_LYR] = LAYOUT_split_3x5_2
+(
+KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,
+KC_GRV ,KC_BSLS,KC_SLSH,KC_MINS,_______,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,
+KC_INS ,KC_LBRC,KC_RBRC,KC_EQL ,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,
+                        _______,KC_SPC ,_______,_______
+),
+[MFA_LYR] = LAYOUT_split_3x5_2
+(
+KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,
+KC_BTN6,KC_BTN5,KC_BTN4,KC_BTN3,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,
+KC_F11 ,KC_F12 ,KC_F13 ,KC_PSCR,_______,_______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,
+                        _______,_______,KC_BTN1,KC_BTN2
+),
 };
 
-// HOLD_MOUSE_TAP_SPACE will go to mouse layer before TAPPING_TERM elapses
+// ATM_SPC will go to mouse layer before TAPPING_TERM elapses
 // if another key is pressed then released
 bool get_permissive_hold(uint16_t keycode,
                          keyrecord_t* record)
 {
     switch (keycode)
     {
-        case HOLD_MOUSE_TAP_SPACE:
+        case ATM_SPC:
             return true;
         default:
             return false;
     }
 }
 
-// HOLD_NUMBE_TAP_SPACE will go to number layer before TAPPING_TERM
+// ATN_SPC will go to number layer before TAPPING_TERM
 // elapses if another key is pressed
 bool get_hold_on_other_key_press(uint16_t keycode,
                                  keyrecord_t* record)
 {
     switch (keycode)
     {
-        case HOLD_NUMBE_TAP_SPACE:
+        case ATN_SPC:
             return true;
         default:
             return false;
